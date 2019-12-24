0a9d56efca6abc6b3fece9fd01e0617dff026de6
Swedde <chelovek.raf@yandex.ru>
2019-12-24 21:04:45 +0300

    test.s

---
+++ asm_src/test.s
@@ -0,0 +1,18 @@
+.name "timorinn"
+.comment "toxic"
+live	%1
+ld     4, r2
+st	r4 , 18
+add	r2 , r3 , r4
+sub	r4, r5, r6
+and	%8 , 7 , r4
+or 9, %-8 , r10
+xor	0, %0, r8
+zjmp %123
+ldi %2 , %2 , r5
+sti	r7 , %5 , %4
+fork %17
+lld %-10 , r5
+lldi %4 , %4 , r1
+lfork %80
+aff r40
